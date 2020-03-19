9368a9a97896da286b7b8fbb6bf5ff4fb7c6a48d
Mikhail Galaktionov <mgalaktionov@lab50.net>
2020-03-19 18:38:39 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Base64;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //TODO: создайте сканнер используя System.in в качестве параметра
+        //Scanner in = ;
+        //TODO: считайте строку пользовательского ввода методом .next()
+        //String originalInput = in./*МЕТОД ДЛЯ СЧИТЫВАНИЯ*/;
+        //System.out.println("оригинальная строка " + originalInput);
+
+        //TODO: конвертируйте строку originalInput в массив байт методом getBytes()
+        //byte[] originalBytes = ;
+        //TODO: создайте объект Encoder используя метод getEncoder()
+        //Base64.Encoder encoder = ;
+        //TODO: используя метод encodeToString() и массив originalBytes
+        // закодируйте введенное пользователем сообщение
+        //String encodedString = encoder./*МЕТОД КОДИРОВАНИЯ В СТРОКУ*/(/*ОРИГИНАЛЬНЫЙ МАССИВ БАЙТ*/);
+        //System.out.println("закодированная строка "+encodedString);
+
+        //TODO: используя противоположные объект и метод (Base64.getDecoder().decode())
+        // декодируйте строку и выведете ее на экран
+        System.out.println(/*ЦЕПОЧКА ВЫЗОВОВ, КОТОРАЯ ДЕКОДИРУЕТ СТРОКУ*/);
+    }
+}
